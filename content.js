5cd01f1c5a1339147dfc02def50415582e1748fc
Soham Goswami <sohamgoswami07@gmail.com>
2024-08-12 10:15:09 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,17 +1,19 @@
 function checkForAds() {
   let adExist = document.getElementsByClassName("ad-showing").length > 0;
   let video = document.getElementsByClassName("video-stream html5-main-video")[0];
+  let skipButton = document.getElementsByClassName("ytp-skip-ad-button")[0];
 
   if (adExist && video) {
     video.playbackRate = 9.5;
+    video.muted = true;
   }
 
-  let skipButton = document.getElementsByClassName("ytp-ad-skip-button-modern ytp-button")[0];
-  if (skipButton && !skipButton.clicked) {
+  if ((skipButton && !skipButton.clicked) || (skipButton != undefined && skipButton.length > 0)) {
     skipButton.click();
     skipButton.clicked = true;
     chrome.runtime.sendMessage({ adsSkipped: true });
-  } else if (!adExist) {
+  }
+  else if (!adExist) {
     if (skipButton) skipButton.clicked = false;
   }
 }
